eb7b0328fed6292fd8d3fd7b6d595960f0aff7d3
Job van der Voort <job@gitlab.com>
2014-02-25 14:54:22 +0100

    redirect to root; remove links in navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to @post, notice: 'Post was successfully created.'
+      redirect_to root_path, notice: 'Post was successfully created.'
     else
       render action: 'new'
     end
