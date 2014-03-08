4c45bf30e7064a888cffb41eef9a580b84e82cbb
Job van der Voort <job@gitlab.com>
2014-03-08 12:59:32 +0100

    board member wip

--- test/integration/sign_in_as_board_member_test.rb
+++ test/integration/sign_in_as_board_member_test.rb
@@ -6,5 +6,17 @@ class SignInAsBoardMemberTest < ActionDispatch::IntegrationTest
     @user = create(:board_member)
   end
 
+  test 'board member log in' do
+    visit root_path
 
+    fill_in 'user_email', with: 'board@member.com'
+    fill_in 'user_password', with: 'jobiscool'
+
+    click_on 'Log in'
+
+    assert page.has_content? 'John'
+    assert page.has_content? 'bestuurslid'
+
+    refute page.has_content? 'Admin'
+  end
 end
