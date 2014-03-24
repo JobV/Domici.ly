4f81df0f57c6dc10595c7ad5b28786f8f372c7cb
Job van der Voort <job@gitlab.com>
2014-03-25 00:11:29 +0100

    remove unn. error

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    Bugsnag.notify 'Someone visited the dashboard'
     @alerts = current_user.hoa ? current_user.hoa.alerts.order(created_at: :desc) : current_user.alerts.order(created_at: :desc)
   end
 end
