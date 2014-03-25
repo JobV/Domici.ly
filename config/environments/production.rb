e842fd961746d0936166b6a4909177ef5a6737d0
Job van der Voort <jobvandervoort@gmail.com>
2014-03-25 23:27:00 +0100

    set from: email correctly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'jaxons.nl' }
+  config.action_mailer.default_url_options = { :host => 'app.domici.ly' }
   config.action_mailer.smtp_settings = {
       :port =>           '587',
       :address =>        'smtp.mandrillapp.com',
