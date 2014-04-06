0b2fafcbcda048dd2b509e3bffc34b204c82f76c
Job van der Voort <jobvandervoort@gmail.com>
2014-04-06 11:53:32 +0200

    states in dutch

--- app/helpers/alerts_helper.rb
+++ app/helpers/alerts_helper.rb
@@ -16,4 +16,11 @@ module AlertsHelper
     return I18n.t('alert.in_progress') if alert_in_progress?(alert)
     return I18n.t('alert.completed') if alert_completed?(alert)
   end
+
+  def progress_name_from_string(string)
+    return I18n.t('alert.new')            if string == 'new'
+    return I18n.t('alert.in_progress')    if string == 'in progress'
+    return I18n.t('alert.completed')      if string == 'completed'
+    return I18n.t('alert.unknown_state')
+  end
 end
