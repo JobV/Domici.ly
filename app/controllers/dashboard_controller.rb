09ed65b358f9da0c300f4fb1e44b28b92d86c826
Job van der Voort <jobvandervoort@gmail.com>
2014-04-14 21:31:45 +0200

    click on moderator to visit his page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    current_user.hoa ? @activities = PublicActivity::Activity.where(hoa_id: current_user.hoa.id).order('created_at desc').limit(10) : @activities = []
+    current_user.hoa ? @activities = PublicActivity::Activity.where(hoa_id: current_user.hoa.id).order('created_at desc').limit(20) : @activities = []
     @post = Post.new user: current_user
   end
 end
