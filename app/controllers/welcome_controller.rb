a5055f7d302fc90326f8017818362c386243a004
Job van der Voort <jobvandervoort@gmail.com>
2014-05-24 12:13:08 +0200

    bug fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   before_filter :authenticate_user!
 
   def welcome
-    redirect_to dashboard_index_path if current_user.hoa
+    redirect_to organisation_path if current_user.hoa
   end
 end
