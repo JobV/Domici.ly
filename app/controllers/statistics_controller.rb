f262ad8945270078c86eef5995e9e90f75c234fe
Job van der Voort <jobvandervoort@gmail.com>
2014-05-24 12:18:46 +0200

    stats api update

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,15 +1,17 @@
 class StatisticsController < ApplicationController
   def user_count
-    msg = { :users => "#{User.count}" }
+    msg = { count: User.count, data: [{timestamp: Time.now, value: User.count}]}
     respond_to do |format|
       format.json { render json: msg}
+      format.text { render text: User.count}
     end
   end
 
   def hoa_count
-    msg = { :hoas => "#{Hoa.count}" }
+    msg = { count: Hoa.count, data: [{timestamp: Time.now, value: Hoa.count}]}
     respond_to do |format|
       format.json { render json: msg}
+      format.text { render text: Hoa.count}
     end
   end
 end
