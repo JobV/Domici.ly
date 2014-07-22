edf1b67e5ac1c68b3430fe1971d7037576bd3776
Job van der Voort <jobvandervoort@gmail.com>
2014-07-22 23:43:51 +0200

    hotfix

--- db/migrate/20140721155800_add_hoa_id_to_attachments.rb
+++ db/migrate/20140721155800_add_hoa_id_to_attachments.rb
@@ -2,5 +2,4 @@ class AddHoaIdToAttachments < ActiveRecord::Migration
   def change
     add_column :attachments, :hoa_id, :integer
   end
-  add_foreign_key(:attachments, :hoa, dependent: :delete)
 end
